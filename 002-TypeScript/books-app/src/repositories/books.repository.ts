b379ec57134f744918cf6dd912da25ba73fd7fca
Денис Гарпинич <d.garpinich@wisetech.pro>
2025-09-07 19:12:48 +0300

    Выполнено задание 002-TypeScript

---
+++ 002-TypeScript/books-app/src/repositories/books.repository.ts
@@ -0,0 +1,13 @@
+import Book from "../interfaces/book.interface";
+
+export default abstract class BooksRepository {
+  abstract createBook(book: Book): void;
+
+  abstract getBook(id: number): Book | null;
+
+  abstract getBooks(): Book[];
+
+  abstract updateBook(id: number, updatedBook: Book): void;
+
+  abstract deleteBook(id: number): void;
+}
