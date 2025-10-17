5f8f15cbb0dcd85683bbb580b45b6547b57cc6cf
Денис Гарпинич <d.garpinich@wisetech.pro>
2025-10-17 19:48:56 +0300

    Выполнено задание 009

--- 009-rxjs/src/rxjs/rxjs.service.ts
+++ 009-rxjs/src/rxjs/rxjs.service.ts
@@ -12,22 +12,37 @@ import axios from "axios";
 
 @Injectable()
 export class RxjsService {
-  private readonly githubURL = "https://api.github.com/search/repositories?q=";
+  private readonly GITHUB_URL = "https://api.github.com/search/repositories?q=";
+  private readonly GITLAB_URL = "https://gitlab.com/api/v4/projects?search=";
 
   private getGithub(text: string, count: number): Observable<any> {
-    return from(axios.get(`${this.githubURL}${text}`))
+    return from(axios.get(`${this.GITHUB_URL}${text}`))
       .pipe(
         map((res: any) => res.data.items),
         mergeAll(),
-      )
-      .pipe(take(count));
+        take(count),
+      );
   }
 
+  private getGitlab(text: string, count: number): Observable<any> {
+    return from(axios.get(`${this.GITLAB_URL}${text}`))
+      .pipe(
+        map((res: any) => res.data),
+        mergeAll(),
+        take(count),
+      );
+  }
+  
   async searchRepositories(text: string, hub: string): Promise<any> {
     // Здесь можно добавить логику проверки на какой hub делать запрос
+    console.log("text = ", text);
     console.log("hub = ", hub);
-    const data$ = this.getGithub(text, 10).pipe(toArray());
-    data$.subscribe(() => {});
+    let data$: Observable<any>;
+    if (hub === "github") {
+      data$ = this.getGithub(text, 10).pipe(toArray());
+    } else if (hub === "gitlab") {
+      data$ = this.getGitlab(text, 10).pipe(toArray());
+    }
     return await firstValueFrom(data$);
   }
 }
