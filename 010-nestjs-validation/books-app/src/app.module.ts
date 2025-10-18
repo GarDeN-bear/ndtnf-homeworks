53d6bd8b3f8d0b2e6fcb2a5d6f62baa362ce1bce
Денис Гарпинич <d.garpinich@wisetech.pro>
2025-10-18 19:45:48 +0300

    Выполнено 010-nestjs-validation

---
+++ 010-nestjs-validation/books-app/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_CONNECTION || 'mongodb://localhost:27017/books',
+    ),
+    BooksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
